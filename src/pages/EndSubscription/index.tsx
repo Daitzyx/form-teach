13490b2d9fdc1b43b6a831f31b21437888ab3194
lcht1 <manuleichtveis@gmail.com>
2022-11-17 17:44:51 -0300

    feat: end subscription page

--- src/pages/EndSubscription/index.tsx
+++ src/pages/EndSubscription/index.tsx
@@ -1,6 +1,63 @@
-import React from 'react'
-import { Container } from './styles'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import {
+  Attention,
+  Container,
+  Content,
+  SocialMedias,
+  Info,
+  ContactInfo
+} from './styles'
+import checkBox from '../../assets/checkbox2.png'
 
 export const EndSubscription = () => {
-  return <Container>EndSubscription</Container>
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content>
+          <Info>
+            <h1>Obrigado por chegar até aqui!</h1>
+            <p>
+              Pronto, você quase concluiu sua inscrição em nossa seleção da 3ª
+              edição do 3035TEACH
+            </p>
+            <p>
+              Fique atento ao seu e-mail cadastrado (atualize a caixa de entrada
+              e verifique o spam), pois entraremos em contato por lá com
+              informações sobre as próximas etapas.
+            </p>
+            <p>
+              Caso tenha alguma dúvida, por favor, entre em contato pelo e-mail:
+              teach@3035tech.com.
+            </p>
+            <p>Para concluir sua inscrição, realize o Teste de lógica: </p>
+          </Info>
+          <Button width="264px">Teste de Lógica</Button>
+          <Attention>
+            ATENÇÃO: é um teste rápido (cerca de 12 minutos) e que necessita de
+            perfil ‘learner’ no site:
+            <a href="https://app.testcrackr.com/">
+              https://app.testcrackr.com/
+            </a>
+            . Caso ainda não tenha login, sugerimos que faça a criação antes de
+            iniciar o teste
+          </Attention>
+        </Content>
+
+        <div>
+          <ContactInfo>
+            <img src={checkBox} alt="ícone de checkbox" />
+            <span>Hey, segue a gente nas redes!</span>
+            <SocialMedias>
+              <span>3035tech.com</span>
+              <span>3035Tech</span>
+              <span>3035Tech</span>
+              <span>3035 Tech</span>
+            </SocialMedias>
+          </ContactInfo>
+        </div>
+      </Container>
+    </>
+  )
 }
--- src/pages/EndSubscription/styles.ts
+++ src/pages/EndSubscription/styles.ts
@@ -3,11 +3,61 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-
   scroll-snap-align: start;
   scroll-snap-stop: always;
-
   display: flex;
   align-items: center;
   justify-content: center;
+`
+
+export const Content = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-left: 100px;
+  line-height: 1.7;
+`
+export const Info = styled.div`
+  width: 60%;
+  font-size: 20px;
+
+  h1 {
+    margin-top: 80px;
+    margin-bottom: 40px;
+    font-size: 50px;
+  }
+
+  p {
+    margin-bottom: 20px;
+  }
+`
+
+export const Attention = styled.span`
+  font-size: 14px;
+  width: 80%;
+
+  a {
+    color: inherit;
+    text-decoration: underline;
+  }
+`
+
+export const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  margin-right: 200px;
+
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+export const SocialMedias = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
 `
